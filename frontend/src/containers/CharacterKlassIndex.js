a7f1c7dc343ef8880c151bdb077cb4d56dc43408
trevismcgill <trevismcgill25@gmail.com>
2020-12-31 13:48:36 -0600

    Added fuzzy search to Character Class Index Page

--- frontend/src/containers/CharacterKlassIndex.js
+++ frontend/src/containers/CharacterKlassIndex.js
@@ -3,15 +3,30 @@ import { connect } from "react-redux";
 import { Container, Row } from "react-bootstrap";
 import KlassCard from "../components/KlassCard";
 class CharacterKlassIndex extends Component {
+	state = {
+		search: "",
+	};
 	render() {
 		return (
 			<div>
 				<h1>Character Class Index</h1>
+				<input
+					type="search"
+					onChange={(e) =>
+						this.setState(({ search }) => ({
+							search: e.target.value,
+						}))
+					}
+					value={this.state.search}
+					placeholder="search Classes"
+				/>
 				<Container>
 					<Row md={4}>
-						{this.props.klasses.map((klass) => (
-							<KlassCard klass={klass} />
-						))}
+						{this.props.klasses.map((klass) =>
+							klass.index.includes(this.state.search) ? (
+								<KlassCard klass={klass} />
+							) : null
+						)}
 					</Row>
 				</Container>
 			</div>
